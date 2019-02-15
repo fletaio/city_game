8aaba58e8edcd1ca5e5cb58f8fcbc8a56450f451
Sam Jeong <judasowelu@firstchain.co>
2019-02-15 23:35:41 +0900

    fix ws address

--- html/resource/js/event.js
+++ html/resource/js/event.js
@@ -199,7 +199,7 @@ function getTileFromPoint(point) {
 }
 
 function connectToServer (addr) {
-	var wsUri = "ws://localhost:8080/websocket/"+addr;
+	var wsUri = "ws://220.76.209.238:8080//websocket/"+addr;
 	function connect() {
 		var ws = new WebSocket(wsUri)
 		ws._init = false;
